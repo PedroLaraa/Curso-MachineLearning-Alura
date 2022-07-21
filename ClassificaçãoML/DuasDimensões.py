ad681c7a652f8db8e62a3ab8993d7bb1c66ad8bc
Pedro Lara <pedroalveslara@gmail.com>
2022-07-21 17:36:25 -0300

    Curva de decisão

--- ClassificaçãoML/DuasDimensões.py
+++ ClassificaçãoML/DuasDimensões.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
+import numpy as np
+
 import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 uri = "https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv"
 
@@ -14,7 +22,52 @@ map = {
 
 dados.rename(columns=map, inplace=True)
 
+troca = {
+    0 : 1,
+    1 : 0
+}
+dados['finalizado'] = dados.nao_finalizado.map(troca)
+
 print(dados.head())
 
-sns.scatterplot(x='horas_esperadas', y='preco', data=dados)
+x = dados[['horas_esperadas', 'preco']]
+y = dados['finalizado']
 
+SEED = 20
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, random_state = SEED, test_size = 0.25, stratify = y)
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+modelo = LinearSVC()
+modelo.fit(treino_x, treino_y)
+previsoes = modelo.predict(teste_x)
+
+previsoes_de_base = np.ones(540)
+
+acuracia = accuracy_score(teste_y, previsoes_de_base) * 100
+print("A acurácia do baseline foi %.2f%%" % acuracia)
+
+x_min = teste_x.horas_esperadas.min()
+x_max = teste_x.horas_esperadas.max()
+y_min = teste_x.preco.min()
+y_max = teste_x.preco.max()
+
+print(x_min, x_max, y_min, y_max)
+
+pixel = 100
+
+eixo_x = np.arange(x_min, x_max, (x_max - x_min) / pixel)
+eixo_y = np.arange(y_min, y_max, (y_max - y_min) / pixel)
+
+xx, yy = np.meshgrid(eixo_x, eixo_y)
+xx.ravel()
+
+pontos = np.c_[xx.ravel(), yy.ravel()]
+
+Z = modelo.predict(pontos)
+Z = Z.reshape(xx.shape)
+
+plt.contourf(xx, yy, Z ,alpha=0.4)
+plt.scatter(teste_x.horas_esperadas, teste_x.preco, c=teste_y, s=1)
+
+plt.show()
