0d0f979014fefca9bf10b3c1aaa58a3bf2c37dd7
Pedro Lara <pedroalveslara@gmail.com>
2022-07-21 15:40:36 -0300

    Classificações

---
+++ ClassificaçãoML/classAcessos.py
@@ -0,0 +1,38 @@
+
+from cgi import test
+import pandas as pd
+
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+uri = 'https://gist.githubusercontent.com/guilhermesilveira/2d2efa37d66b6c84a722ea627a897ced/raw/10968b997d885cbded1c92938c7a9912ba41c615/tracking.csv'
+dados = pd.read_csv(uri)
+
+mapa = {
+    'home': 'principal',
+    'how_it_works': 'como_funciona',
+    'contact': 'contato',
+    'bought': 'comprou'
+}
+
+dados.rename(columns=mapa, inplace=True)
+
+x = dados[["principal", "como_funciona", "contato"]]
+y = dados[["comprou"]]
+
+print('-----------------------------------------------------')
+print(x.head())
+print('-----------------------------------------------------')
+print(y.head())
+SEED = 20
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, random_state = SEED, test_size = 0.25, stratify = y)
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+modelo = LinearSVC()
+modelo.fit(treino_x, treino_y)
+previsoes = modelo.predict(teste_x)
+
+acuracia = accuracy_score(teste_y, previsoes) * 100
+print("A acurácia foi %.2f%%" % acuracia)
--- ClassificaçãoML/classAnimais.py
+++ ClassificaçãoML/classAnimais.py
@@ -1,5 +1,6 @@
-
 from sklearn.svm import LinearSVC
+
+from sklearn.metrics import accuracy_score
 
 # Features
 # 1. Pelo longo
@@ -14,10 +15,34 @@ cachorro1 = [0, 1, 1]
 cachorro2 = [1, 0, 1]
 cachorro3 = [1, 1, 1]
 
-dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
-
-classes = [1, 1, 1, 0, 0, 0]
+treino_x = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
+treino_y = [1, 1, 1, 0, 0, 0]
 
 model = LinearSVC()
 
-model.fit(dados, classes)
+model.fit(treino_x, treino_y)
+animal_misterioso = [1, 1, 1]
+
+print(model.predict([animal_misterioso]))
+
+misterio1 = [1,1,1]
+
+misterio2 = [0,0,1]
+
+misterio3 = [1,1,0]
+
+teste_x = [misterio1, misterio2, misterio3]
+
+previsoes = model.predict(teste_x)
+
+print(previsoes)
+
+teste_y = [0, 1, 1]
+
+corretos = (previsoes == teste_y).sum()
+total = len(teste_x)
+taxa_de_acerto = corretos / total
+print("Taxa de acerto: %.2f" % (taxa_de_acerto * 100))
+
+taxa_de_acerto = accuracy_score(teste_y, previsoes)
+print("Taxa de acerto: %.2f" % (taxa_de_acerto * 100))
