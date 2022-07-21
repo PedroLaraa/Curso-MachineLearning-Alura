356a90a4d88b1e37e989da58ca4ba09d673e37b7
Pedro Lara <pedroalveslara@gmail.com>
2022-07-21 13:22:51 -0300

    Create classAnimais.py

---
+++ ClassificaçãoML/classAnimais.py
@@ -0,0 +1,17 @@
+
+from sklearn.svm import linearSVC
+
+# Features
+# 1. Pelo longo
+# 2. Perna curta
+# 3. Faz auau
+
+porco1 = [0, 1, 0]
+porco2 = [0, 1, 1]
+porco3 = [1, 1, 0]
+
+cachorro1 = [0, 1, 1]
+cachorro2 = [1, 0, 1]
+cachorro3 = [1, 1, 1]
+
+dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
