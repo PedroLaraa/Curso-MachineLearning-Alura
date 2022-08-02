6e153ef7ca9a7e3a233e873af7ca821bd23af04a
Pedro Lara <pedroalveslara@gmail.com>
2022-08-02 09:49:41 -0300

    Update classificaBuscas.py

--- Parte II - Machine Learning/classificaBuscas.py
+++ Parte II - Machine Learning/classificaBuscas.py
@@ -1,8 +1,39 @@
-from tkinter import Y
 import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
 
-dados = pd.read_csv('cursos.csv')
-print(dados.head())
+df = pd.read_csv('cursos.csv')
+X_df = df[['home', 'busca', 'logado']]
+Y_df = df['comprou']
 
-X = dados[['home','busca','logado']]
-Y = dados['comprou']
+Xdummies_df = pd.get_dummies(X_df).astype(int)
+Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+porcentagem_treino = 0.9
+
+tamanho_de_treino = int(porcentagem_treino * len(Y))
+tamanho_de_teste = len(Y) - tamanho_de_treino
+
+treino_dados = X[:tamanho_de_treino]
+treino_marcacoes = Y[:tamanho_de_treino]
+
+teste_dados = X[-tamanho_de_teste:]
+teste_marcacoes = Y[-tamanho_de_teste:]
+
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(taxa_de_acerto)
+print(total_de_elementos)
