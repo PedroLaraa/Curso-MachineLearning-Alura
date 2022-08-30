8733d51c4da21228bc81f61a644e67e2eb278866
Pedro Lara <pedroalveslara@gmail.com>
2022-08-30 11:53:06 -0300

    AdaBoostClassifier

--- Parte II - Machine Learning/classificaBuscas.py
+++ Parte II - Machine Learning/classificaBuscas.py
@@ -1,22 +1,15 @@
-
 import pandas as pd
-from sklearn.naive_bayes import MultinomialNB
+from collections import Counter
 
 df = pd.read_csv('cursos.csv')
-X_df = df[['home', 'busca', 'logado']]
+X_df = df[['home','busca', 'logado']]
 Y_df = df['comprou']
 
-Xdummies_df = pd.get_dummies(X_df).astype(int)
+Xdummies_df = pd.get_dummies(X_df)
 Ydummies_df = Y_df
 
 X = Xdummies_df.values
-Y = Ydummies_df.values
-
-# a eficácia do algoritmo que chuta tudo 0 ou 1
-acerto_de_um = sum(Y)
-acerto_de_zero = len(Y) - acerto_de_um
-taxa_de_acerto_base = 100.0 * max(acerto_de_um, acerto_de_zero) / len(Y)
-print("Taxa de acerto base: %f" % taxa_de_acerto_base)
+Y = Ydummies_df.values 
 
 porcentagem_treino = 0.9
 
@@ -29,21 +22,34 @@ treino_marcacoes = Y[:tamanho_de_treino]
 teste_dados = X[-tamanho_de_teste:]
 teste_marcacoes = Y[-tamanho_de_teste:]
 
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes):
+    modelo.fit(treino_dados, treino_marcacoes)
+
+    resultado = modelo.predict(teste_dados)
+
+    acertos = resultado == teste_marcacoes
+
+    total_de_acertos = sum(acertos)
+    total_de_elementos = len(teste_dados)
+
+    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+    msg = "Taxa de acerto do algoritmo {0}: {1}".format(nome, taxa_de_acerto)
+
+    print(msg)
+
+
+from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
-modelo.fit(treino_dados, treino_marcacoes)
+fit_and_predict("MultinomialNB", modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
 
-resultado = modelo.predict(teste_dados)
+from sklearn.ensemble import AdaBoostClassifier
+modelo = AdaBoostClassifier()
+fit_and_predict("AdaBoostClassifier", modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
 
-diferencas = resultado - teste_marcacoes
+acerto_base = max(Counter(teste_marcacoes).values())
+taxa_de_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
 
-acertos = [d for d in diferencas if d == 0]
-total_de_acertos = len(acertos)
 total_de_elementos = len(teste_dados)
-
-taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
-
-print('-------------------------------------------')
-print('Testando com ',total_de_elementos, ' elementos')
-print('-------------------------------------------')
-print('Taxa de acerto: ',taxa_de_acerto, '%')
-print('-------------------------------------------')
+print("Total de teste: %d" % total_de_elementos)
