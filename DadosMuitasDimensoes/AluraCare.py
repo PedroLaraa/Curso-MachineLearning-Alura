c269b0e3fc51280e2cd8c6caf3ca5834afced1a2
Pedro Lara <pedroalveslara@gmail.com>
2022-08-31 16:24:57 -0300

    Update AluraCare.py

--- DadosMuitasDimensoes/AluraCare.py
+++ DadosMuitasDimensoes/AluraCare.py
@@ -6,10 +6,12 @@ from sklearn.dummy import DummyClassifier
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.feature_selection import RFECV
 
 resultados_dos_exames = pd.read_csv('exames.csv')
 
-SEED = 123143
+SEED = 1234
 random.seed(SEED)
 
 valores_exames = resultados_dos_exames.drop(columns=['id', 'diagnostico'])
@@ -17,11 +19,13 @@ diagnostico = resultados_dos_exames.diagnostico
 
 valores_exames_v1 = valores_exames.drop(columns= "exame_33")
 
-treino_x, teste_x, treino_y, teste_y = train_test_split(valores_exames_v1, diagnostico, test_size=0.3)
+valores_exames_v6 = valores_exames_v1.drop(columns=(["exame_4", "exame_29", "exame_3", "exame_24"]))
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(valores_exames_v6, diagnostico, test_size=0.3)
 
 classificador = RandomForestClassifier(n_estimators= 100)
 classificador.fit(treino_x, treino_y)
-print('Resultado da classificação: %.2f%% ' %(classificador.score(teste_x, teste_y) *100))
+# print('Resultado da classificação: %.2f%% ' %(classificador.score(teste_x, teste_y) *100))
 
 classificador_bobo = DummyClassifier(strategy='most_frequent')
 classificador_bobo.fit(treino_x, treino_y)
@@ -35,27 +39,54 @@ valores_exames_v2 = pd.DataFrame(data= valores_exames_v2, columns=valores_exames
 
 valores_exames_v3 = valores_exames_v2.drop(columns=['exame_29', 'exame_4'])
 
-def classificar(valores):
+# def classificar(valores):
 
-    treino_x, teste_x, treino_y, teste_y = train_test_split(valores, diagnostico, test_size=0.3)
+#     treino_x, teste_x, treino_y, teste_y = train_test_split(valores, diagnostico, test_size=0.3)
 
-    classificador = RandomForestClassifier(n_estimators= 100)
-    classificador.fit(treino_x, treino_y)
-    print('Resultado da classificação: %.2f%% ' %(classificador.score(teste_x, teste_y) *100))
+#     classificador = RandomForestClassifier(n_estimators= 100)
+#     classificador.fit(treino_x, treino_y)
+#     print('Resultado da classificação: %.2f%% ' %(classificador.score(teste_x, teste_y) *100))
 
-def grafico_violino(valores, inicio, fim):
+# def grafico_violino(valores, inicio, fim):
 
-    dados_plot = pd.concat([diagnostico, valores.iloc[:,inicio:fim]], axis = 1)
-    dados_plot = pd.melt(dados_plot, id_vars='diagnostico', var_name='exames', value_name='valores')
+#     dados_plot = pd.concat([diagnostico, valores.iloc[:,inicio:fim]], axis = 1)
+#     dados_plot = pd.melt(dados_plot, id_vars='diagnostico', var_name='exames', value_name='valores')
 
-    plt.figure(figsize=(10, 10))
-    sns.violinplot(x='exames', y='valores', hue='diagnostico' ,data=dados_plot, split=True)
+#     plt.figure(figsize=(10, 10))
+#     sns.violinplot(x='exames', y='valores', hue='diagnostico' ,data=dados_plot, split=True)
 
-    plt.xticks(rotation=90)
+#     plt.xticks(rotation=90)
 
-    # EXIBE O GRÁFICO
-    plt.show()
+#     # EXIBE O GRÁFICO
+#     plt.show()
 
-grafico_violino(valores_exames_v2, 10, 21)
+# grafico_violino(valores_exames_v2, 10, 21)
 
-classificar(valores_exames_v3)
+# classificar(valores_exames_v3)
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(valores_exames_v6, diagnostico, test_size = 0.3)
+
+classificador = RandomForestClassifier(n_estimators=100, random_state = 1234)
+classificador.fit(treino_x, treino_y)
+
+selecionador_rfecv = RFECV(estimator = classificador, step = 1, scoring='accuracy')
+selecionador_rfecv.fit(treino_x, treino_y)
+treino_rfecv = selecionador_rfecv.transform(treino_x)
+teste_rfecv = selecionador_rfecv.transform(teste_x)
+classificador.fit(treino_rfecv, treino_y)
+
+# matriz_confusao = confusion_matrix(teste_y, classificador.predict(teste_rfecv))
+# plt.figure(figsize = (10, 8))
+# sns.set(font_scale = 2)
+# sns.heatmap(matriz_confusao, annot = True, fmt = "d").set(xlabel = "Predição", ylabel = "Real")
+
+print("Resultado da classificação %.2f%%" % (classificador.score(teste_rfecv, teste_y)* 100))
+
+plt.figure(figsize=(14, 8))
+plt.xlabel('Número de exames')
+plt.ylabel('Acurácia')
+
+plt.plot(range(1, len(selecionador_rfecv.grid_scores_) +1), selecionador_rfecv.grid_scores_)
+
+# EXIBE O GRÁFICO
+plt.show()
